9ee2bb3390ce29f94f43f728333e7328e5ab2283
esidhu <esidhu@octavebytes.com>
2025-01-18 14:54:08 +0500

    badges added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { NavigationComponent } from './views/partials/navigation/navigation.component';
+import { SummaryComponent } from './views/summary/summary.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavigationComponent],
+  imports: [RouterOutlet,NavigationComponent,SummaryComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
